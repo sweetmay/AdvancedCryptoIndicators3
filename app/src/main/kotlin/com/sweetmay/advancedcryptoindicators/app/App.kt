bb582654d83b3a2848cb2bb3df51959a20f7eaf7
igor.saltykov <igor.saltykov@krit.pro>
2022-07-17 02:38:32 +0400

    add koin

--- app/src/main/kotlin/com/sweetmay/advancedcryptoindicators/app/App.kt
+++ app/src/main/kotlin/com/sweetmay/advancedcryptoindicators/app/App.kt
@@ -2,6 +2,7 @@ package com.sweetmay.advancedcryptoindicators.app
 
 import android.app.Application
 import com.sweetmay.advancedcryptoindicators.data.di.dataModule
+import domainModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -18,7 +19,7 @@ class App: Application() {
         startKoin {
             androidContext(this@App)
             androidLogger(Level.ERROR)
-            modules(appModule + dataModule)
+            modules(appModule + dataModule + domainModule)
         }
     }
 
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -2,11 +2,14 @@ plugins {
     id("java-library")
     id("kotlin")
 }
-dependencies {
-
-}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    val koin: String by rootProject.extra
+
+    implementation(koin)
 }
